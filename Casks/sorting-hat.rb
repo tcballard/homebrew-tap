be41ff57c58006e9855b57795abfb9f2ee158f79
Tom Ballard <tom@armytage.co>
2026-07-12 10:35:54 +0100

    Fix Sorting Hat repository URLs

--- Casks/sorting-hat.rb
+++ Casks/sorting-hat.rb
@@ -2,10 +2,10 @@ cask "sorting-hat" do
   version "0.1.0"
   sha256 "1dcb4e70ceba0b2b10f556e9f4f0939174831971056875974cfe88c5b33f8395"
 
-  url "https://github.com/tcballard/Meanwhile/releases/download/v#{version}/Sorting-Hat-v#{version}.zip"
+  url "https://github.com/tcballard/SortingHat/releases/download/v#{version}/Sorting-Hat-v#{version}.zip"
   name "Sorting Hat"
   desc "AI-powered drop folder and Finder Quick Action for organizing files"
-  homepage "https://github.com/tcballard/Meanwhile"
+  homepage "https://github.com/tcballard/SortingHat"
 
   depends_on macos: :sonoma
 
